282ba03792ce4623b264002a61127c1ed5b5f318
David Cap <david.cap@bocians.net>
2016-12-01 13:50:07 -0800

    quotes fix on import statement

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ClientService} from '../services/client.service';
-import {Client} from "../models/client.model";
+import {Client} from '../models/client.model';
 
 @Component({
     selector: 'direct-services-clients',
